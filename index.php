06fe314c62a88db8d9b4cc3ae790c88f1ddd56ac
Faiz Kurniawan <mbfzak1@gmail.com>
2024-04-23 12:00:58 +0700

    Disable warning messages

--- index.php
+++ index.php
@@ -305,5 +305,9 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
-error_reporting(E_ALL & ~E_DEPRECATED);
+
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('log_errors', 1);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 require_once BASEPATH.'core/CodeIgniter.php';
