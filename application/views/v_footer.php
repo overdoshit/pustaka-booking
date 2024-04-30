589d5f926bf7a05677eb50a9e01a9facf93547fc
Faiz Kurniawan <mbfzak1@gmail.com>
2024-04-30 13:43:59 +0700

    Fix web pertemuan 4

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <a href="http://www.RentalBuku.com">RentalBuku  </a>
+        <a href="http://www.RentalBuku.com">RentalBuku</a>
     </footer>
     </div>
 </body>
--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css"> </head> <body>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
 </head>
 <body>
     <div id="wrapper">
